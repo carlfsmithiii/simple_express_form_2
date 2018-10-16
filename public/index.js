4374a4d1179102366354d5dfd12cae89e80e05cb
Carl Smith <carlfsmithiii@gmail.com>
2018-10-16 11:41:28 -0400

    assignment -- added more form fields

--- public/index.js
+++ public/index.js
@@ -1,44 +1,76 @@
 const userCreateForm = document.getElementById("user-create-form")
 const userCreateSubmitButton = userCreateForm.querySelector("button[type='submit']")
-const submissionSuccessDisplayNode = document.getElementById("submission-status"); 
+const submissionSuccessDisplayNode = document.getElementById("submission-status");
 
 userCreateSubmitButton.addEventListener('click', (event) => {
     event.preventDefault();
     const username = document.querySelector('input[name="username"]').value;
     const email = document.querySelector('input[name="email"]').value;
-    const zip_code = document.querySelector('input[name="zip-code"]').value;
-    const fav_color = document.querySelector('input[name="fav-color"]').value;
+    const zipCode = document.querySelector('input[name="zip-code"]').value;
+    const mobileNumber = document.querySelector('input[name="mobile-number"]').value;
+    const socialMedia = document.querySelector('input[name="social-profile"]').value;
+    const preferredCommMethod = (() => {
+        const contactMethodRadioButtons = document.getElementsByName('preferred-contact-method-selector');
+        for (let radioButton of contactMethodRadioButtons) {
+            if (radioButton.checked) {
+                return radioButton.value;
+            }
+        }
+    })();
+    const userDevices = (() => {
+        const devices = [];
+        const userDevicesCheckBoxes = document.getElementsByName('user-devices');
+        for (let checkbox of userDevicesCheckBoxes) {
+            if (checkbox.checked) {
+                devices.push(checkbox.value);
+            }
+        }
+        return devices;
+    })();
+
+    const userType = (() => {
+        const userTypeSelect = document.getElementById('user-type');
+        return userTypeSelect[userTypeSelect.selectedIndex].value;
+    })();
+
+    const favColor = document.getElementById('fav-color').value;
+
     const User = {
         username,
         email,
-        zip_code,
-        fav_color
+        zipCode,
+        mobileNumber,
+        socialMedia,
+        preferredCommMethod,
+        userDevices,
+        userType,
+        favColor
     };
 
     const jsonUser = JSON.stringify(User);
 
     fetch('/api/user/', {
-        method: "POST",
-        headers: {
-            'Content-Type': "application/json; charset=utf-8"
-        },
-        body: jsonUser
-    }).then(response => {
-        // console.log(response);
-        if (response.status == 201) {
-            console.log("successfully added");
-            const successText = document.createTextNode('user successfully added!');
-            submissionSuccessDisplayNode.innerHTML = "";
-            submissionSuccessDisplayNode.appendChild(successText);
-        } else if (response.status == 409) {
-            console.log('username already in use');
-            const failureText = document.createTextNode('error -- user name already in use');
-            submissionSuccessDisplayNode.innerHTML = '';
-            submissionSuccessDisplayNode.appendChild(failureText);
-        }
-        return response.json();
-    })
-    .then(responseObject => {
-        console.log("response:\n", responseObject);
-    });
+            method: "POST",
+            headers: {
+                'Content-Type': "application/json; charset=utf-8"
+            },
+            body: jsonUser
+        }).then(response => {
+            // console.log(response);
+            if (response.status == 201) {
+                console.log("successfully added");
+                const successText = document.createTextNode('user successfully added!');
+                submissionSuccessDisplayNode.innerHTML = "";
+                submissionSuccessDisplayNode.appendChild(successText);
+            } else if (response.status == 409) {
+                console.log('username already in use');
+                const failureText = document.createTextNode('error -- user name already in use');
+                submissionSuccessDisplayNode.innerHTML = '';
+                submissionSuccessDisplayNode.appendChild(failureText);
+            }
+            return response.json();
+        })
+        .then(responseObject => {
+            console.log("response:\n", responseObject);
+        });
 });
